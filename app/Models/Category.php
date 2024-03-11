ed680323c7063a5812929afd6a356e79bcc2389b
moonmouse11 <mmssikg@gmail.com>
2024-03-11 19:53:31 +0500

    End relations creating

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -4,7 +4,9 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasManyThrough;
 use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
@@ -22,5 +24,20 @@ class Category extends Model
     {
         return $this->hasOne(Bb::class, 'category', 'title')
             ->ofMany('created_at');
+    }
+
+    public function offer(): HasManyThrough
+    {
+        return $this->hasManyThrough(Offer::class, Bb::class);
+    }
+
+    public function categories(): HasMany
+    {
+        return $this->hasMany(self::class, 'parent_id');
+    }
+
+    public function parent(): BelongsTo
+    {
+        return $this->belongsTo(self::class, 'parent_id');
     }
 }
--- database/migrations/2024_03_05_094706_create_categories_table.php
+++ database/migrations/2024_03_05_094706_create_categories_table.php
@@ -14,6 +14,9 @@ return new class extends Migration
         Schema::create('categories', function (Blueprint $table) {
             $table->string('title', 50)->primary();
             $table->text('description');
+            $table->bigInteger('parent_id');
+            $table->foreign('parent_id')->references('id')
+                ->on('categories')->restrictOnDelete();
             $table->timestamps();
         });
 
