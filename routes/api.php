e175ba00ae9d4ecd03a0687796add548732da53d
moonmouse11 <mmssikg@gmail.com>
2024-04-09 23:49:23 +0500

    Add jwt route

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::prefix('v1')->group(function () {
+    Route::get('jwt', function () {
+        return response('jwt here', 200);
+    });
+});
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
